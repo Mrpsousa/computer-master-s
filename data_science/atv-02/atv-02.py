808d5d465ca6f59dbde3247a3ad55b02ef54c6ae
Marcos Rogerio Pinheiro Sousa <mrpsousa@apoioprodesp.sp.gov.br>
2021-08-30 15:56:10 -0300

    adjust: correcaçao no algoritimo

--- data_science/atv-02/atv-02.py
+++ data_science/atv-02/atv-02.py
@@ -27,23 +27,26 @@ temperatura = getValues('Jogar', 'Sim', 'Temperatura', 'Intermediaria')
 umidade = getValues('Jogar', 'Sim', 'Umidade', 'Alta')
 vento_forte = getValues('Jogar', 'Sim', 'Vento', 'Forte')
 
-pb_ensolarado = getProba(dias, ensolarado)
-pb_temp_moderd = getProba(dias, temperatura)
-pb_umid_alta = getProba(dias, umidade)
-pb_vent_fort = getProba(dias, vento_forte)
-pb_jogar_tenis = getProba(dias, jogar_sim)
+
+
+pb_ensolarado = getProba(jogar_sim, ensolarado)
+pb_temp_moderd = getProba(jogar_sim, temperatura)
+pb_umid_alta = getProba(jogar_sim, umidade)
+pb_vent_fort = getProba(jogar_sim, vento_forte)
+pb_jogar_tenis = getProba(jogar_sim, dias)
 
 jogar_tenis_final = pb_ensolarado * pb_jogar_tenis * pb_temp_moderd * pb_umid_alta * pb_vent_fort
 print(f'Jogar Tênis: {jogar_tenis_final}')
 
 #--------------------------------------------------------------
+
 # Não jogar tênis
 n_dias = dias - jogar_sim  #14 - 9
 pb_ensolarado_n_jogar = getProba(n_dias, ensolarado)
 pb_temp_moderd_n_jogar = getProba(n_dias, temperatura)
 pb_umid_alta_n_jogar = getProba(n_dias, umidade)
 pb_vent_fort_n_jogar = getProba(n_dias, vento_forte)
-pb_n_jogar_tenis = getProba(n_dias, jogar_sim)
+pb_n_jogar_tenis = getProba(dias, n_dias)
 
 n_jogar_tenis_final = pb_ensolarado_n_jogar * pb_n_jogar_tenis * pb_temp_moderd_n_jogar * pb_umid_alta_n_jogar * pb_vent_fort_n_jogar
 print(f'Não Jogar Tênis: {n_jogar_tenis_final}')
